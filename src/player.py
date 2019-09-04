7895ae031bbec3d5f0f9b1e3b4a4e27cd8460cc9
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-09-04 18:51:28 -0500

    wrote classes for players and rooms

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player(): 
+    def __init__(self, currentRoom)
+        self.currentRoom = currentRoom
+    
+    def __str__(self):
+        return ("current room is: "  self.currentRoom)
+
+        
